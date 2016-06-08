3f355dfa10020fc2239d2a660706ebb6ef5b2293
Charlie Baxter and Shradha Pulla <shradha@>
2016-06-08 16:49:41 -0700

    Add js code to make calculator work

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,28 @@ $(document).ready(function() {
 
     var binaryInput = $("input#binary").val();
     var binarySplit = binaryInput.split("");
-    var result = binarySplit.reverse();
+    var binaryNumbers = binarySplit.reverse();
+    var runningTotal = 0;
 
-    result.forEach(function(binary) {
-      var exponentPower = ;
-      var exponent = 2^(exponentPower);
-      var binaryInteger = parseInt(binaryInput);
-      result = binaryInteger * exponent;
-    });
-    alert(result);
+    for (i = 0; i <= (binaryNumbers.length - 1); i++) {
+      var exponent = Math.pow(2,i);
+      // alert(exponent);
+      var binaryInteger = parseInt(binaryNumbers[i]);
+      // alert(binaryInteger);
+      runningTotal = runningTotal + (binaryInteger * exponent);
+    };
+    alert(runningTotal);
   });
 });
+
+    // binaryNumbers.forEach(function(binary) {
+    //   alert(binary);
+    //   var exponentPower = binaryNumbers.indexOf(binary);
+    //   var exponent = Math.pow(2,exponentPower);
+    //   var binaryInteger = parseInt(binary);
+    //   var runningTotal = runningTotal + (binaryInteger * exponent);
+    //   alert(runningTotal);
+    // });
+
+//   });
+// });
