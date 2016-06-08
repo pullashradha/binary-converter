817730eb591d865bc0afa4030fd640365c7cb084
Charlie Baxter and Shradha Pulla <charlie@>
2016-06-08 16:52:32 -0700

    Deleted comments

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,26 @@ $(document).ready(function() {
 
     for (i = 0; i <= (binaryNumbers.length - 1); i++) {
       var exponent = Math.pow(2,i);
-      // alert(exponent);
       var binaryInteger = parseInt(binaryNumbers[i]);
-      // alert(binaryInteger);
       runningTotal = runningTotal + (binaryInteger * exponent);
     };
     alert(runningTotal);
   });
 });
 
-    // binaryNumbers.forEach(function(binary) {
-    //   alert(binary);
-    //   var exponentPower = binaryNumbers.indexOf(binary);
-    //   var exponent = Math.pow(2,exponentPower);
-    //   var binaryInteger = parseInt(binary);
-    //   var runningTotal = runningTotal + (binaryInteger * exponent);
-    //   alert(runningTotal);
-    // });
 
+
+
+
+// Old Code
+//     binaryNumbers.forEach(function(binary) {
+//       alert(binary);
+//       var exponentPower = binaryNumbers.indexOf(binary);
+//       var exponent = Math.pow(2,exponentPower);
+//       var binaryInteger = parseInt(binary);
+//       var runningTotal = runningTotal + (binaryInteger * exponent);
+//       alert(runningTotal);
+//     });
+//
 //   });
 // });
