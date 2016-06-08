20fcbf49108539e2fa6da46e883045a9f4528dcc
Charlie Baxter and Shradha Pulla <charlie@>
2016-06-08 16:14:57 -0700

    Add basic code, will work on finding definition for exponentPower in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
   $("form#decimal").submit(function(event) {
+    event.preventDefault();
 
+    var binaryInput = $("input#binary").val();
+    var binarySplit = binaryInput.split("");
+    var result = binarySplit.reverse();
 
+    result.forEach(function(binary) {
+      var exponentPower = ;
+      var exponent = 2^(exponentPower);
+      var binaryInteger = parseInt(binaryInput);
+      result = binaryInteger * exponent;
+    });
+    alert(result);
   });
 });
